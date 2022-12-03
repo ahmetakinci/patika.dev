cb888903a01acd655b269ee659c4709b88c93b72
Ahmet Arif Akıncı <ahmetarifaknci@gmail.com>
2022-12-03 19:05:12 +0300

    Polindrom Sayı

---
+++ patika.dev/Palindrom_Sayi/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Bir Sayı Giriniz: ");
+        int a = scanner.nextInt();
+
+        int temp = a, reverse_number = 0, last_number;
+
+        while (temp != 0) {
+            last_number = temp % 10;
+            reverse_number = (reverse_number * 10) + last_number;
+            temp /= 10;
+        }
+        if (reverse_number == a)
+            System.out.println(a + " Sayısı Bir Polindrom Sayıdır");
+        else
+            System.out.println(a + " Sayısı Bir Polindrom Sayı Değildir");
+    }
+}
